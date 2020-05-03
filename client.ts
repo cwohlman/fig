f340eeeb37db7c03fe1bf7c9ce7eb47c1486c150
Joshua Ohlman <codewriterohs@gmail.com>
2020-05-02 20:55:30 -0400

    Add client

--- client.ts
+++ client.ts
@@ -1,9 +1,70 @@
 import { FigHost, FigFeed } from "./feed";
-import { Store, Middleware, PublicKey } from "./server";
-import { SecretKey, FigIssuer } from "./issuer";
+import { Store, Middleware, PublicKey, Record } from "./core";
+import { SecretKey, FigIssuer, Message } from "./issuer";
 
-export default function createClient() {
+export default function createClient(browser: Browser, host: FigHost, middlewares: Middleware[], feedMiddlewares: Middleware[]) {
+  const configurationLocalstorageKey = 'fig.configuration';
+  const configurationStore: ConfigStore = {
+    getConfiguration() {
+      const value = browser.localStorage.getItem(configurationLocalstorageKey);
+      if (value) {
+        return JSON.parse(value) as ClientConfig;
+      }
+      return null;
+    },
+    setConfiguration(config: ClientConfig) {
+      browser.localStorage.setItem(configurationLocalstorageKey, JSON.stringify(config));
+    }
+  };
+  const tokensLocalstorageKey = 'fig.tokens';
+  const tokenStoreBacking = {
+    load() {
+      const value = browser.localStorage.getItem(tokensLocalstorageKey);
+      if (value) {
+        return JSON.parse(value) as Record[];
+      }
+      return [];
+    },
+    save(tokens: Record[]) {
+      browser.localStorage.setItem(tokensLocalstorageKey, JSON.stringify(tokens));
+    },
+  }
+  const tokens = tokenStoreBacking.load();
+  const tokenStore: Store = {
+    getById(id: string) {
+      return tokens.find(t => t.id == id) || null;
+    },
+    query(query) {
+      const limit = query.limit || 100;
+      if (query.after) {
+        const index = tokens.findIndex(t => t.id === query.after);
+        return tokens.slice(index, index + limit);
+      }
 
+      return tokens.slice(-limit);
+    },
+    insert(record) {
+      tokens.push(record);
+      setTimeout(() => tokenStoreBacking.save(tokens), 0);
+    },
+  };
+  return new Client({
+    tokenStore,
+    configurationStore,
+    issuer: host,
+    middlewares,
+    feedMiddlewares,
+  });
+}
+
+export function createScopedClient(scope: string, browser: Browser, host: FigHost) {
+  // Just provide mock localStorage & indexDb which prefix the keys with the scope string
+  throw new Error("Not implemented")
+}
+
+export type Browser = {
+  localStorage: WindowLocalStorage["localStorage"],
+  indexDb: IDBFactory // TODO: use this once we support an async store
 }
 
 export type ConfigStore = {
@@ -52,7 +113,7 @@ export class Client {
     this.params.configurationStore.setConfiguration(this.config);
     this.initializeFeed();
   }
-  
+
   initializeFeed() {
     this.feed = new FigFeed({
       store: this.params.tokenStore,
