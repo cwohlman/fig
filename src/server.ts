5279ee9d77be11b804079c2bb36f3e91e6359462
Joshua Ohlman <codewriterohs@gmail.com>
2020-05-09 10:23:33 -0400

    Mock out fig middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,51 @@
 import { Fig } from "./core";
+import connect, { IncomingMessage } from "connect";
+import bodyparser from "body-parser";
+import { ServerResponse } from "http";
 
 // TODO: should return a middleware from the store
 // TODO: should verify keys
 
-export function createMiddleware() {
+export type FigRequest = IncomingMessage & {
+  body?: string | string[];
+  figMessages?: string[];
+};
 
+export function createFigHandler(source: Fig, warn: (message: string) => void) {
+  function handleFigRequest(req: FigRequest, res: ServerResponse) {
+    if (req.figMessages) {
+      // process messages
+    }
+
+    // return messages
+  }
+
+  return handleFigRequest;
+}
+
+export function messageMiddleware(req: FigRequest, res: ServerResponse) {
+  if (typeof req.body === "string") {
+    req.figMessages = req.body.split("\n").filter((message) => message);
+  } else if (req.body instanceof Array) {
+    req.figMessages = req.body.filter((message) => message);
+  }
+}
+
+export const defaultFigTextBodyParser = bodyparser.text({
+  type: ["text/jwt", "application/jwt", "application/fig", "text/plain"],
+});
+export const defaultFigJsonBodyParser = bodyparser.json();
+
+export default function createMiddleware(
+  source: Fig,
+  warn: (message: string) => void = (message) => console.warn(message)
+) {
+  const chain = connect();
+
+  chain.use(defaultFigTextBodyParser);
+  chain.use(defaultFigJsonBodyParser);
+  chain.use(messageMiddleware);
+  chain.use(createFigHandler(source, warn));
+
+  return chain;
 }
