bd53bcdfd6de1c1bfca4a9bd991587e2e4de7215
Joshua Ohlman <codewriterohs@gmail.com>
2020-05-09 10:33:31 -0400

    Basic middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { Fig } from "./core";
+import { Fig, Query, Record } from "./core";
 import connect, { IncomingMessage } from "connect";
 import bodyparser from "body-parser";
 import { ServerResponse } from "http";
@@ -8,18 +8,43 @@ import { ServerResponse } from "http";
 export type FigRequest = IncomingMessage & {
   body?: string | string[];
   figMessages?: string[];
+  figQuery?: Query;
 };
 
-export function createFigHandler(source: Fig, warn: (message: string) => void) {
+export function createFigHandler(source: Fig, warn: (message: Error) => void) {
   function handleFigRequest(req: FigRequest, res: ServerResponse) {
     if (req.figMessages) {
-      // process messages
+      source.accept(req.figMessages);
     }
 
-    // return messages
+    if (req.method === "POST" && !req.figMessages) {
+      warn(
+        new Error(
+          "Expected a middleware to provide req.figMessages, try using messageMiddleware or createDefaultHandler"
+        )
+      );
+    }
+
+    const messages = source.query(req.figQuery || {});
+
+    if (!req.figQuery) {
+      warn(
+        new Error(
+          "Expected a middleware to provide req.figQuery, try using queryMiddleware or createDefaultHandler"
+        )
+      );
+    }
+
+    res.setHeader("content-type", "application/fig");
+    res.statusCode = 200;
+    res.end(formatMessages(messages));
   }
 
   return handleFigRequest;
+}
+
+export function formatMessages(messages: Record[]): string {
+  return messages.map((m) => m.token).join("\n");
 }
 
 export function messageMiddleware(req: FigRequest, res: ServerResponse) {
@@ -30,14 +55,16 @@ export function messageMiddleware(req: FigRequest, res: ServerResponse) {
   }
 }
 
+export function queryMiddleware(req: FigRequest, res: ServerResponse) {}
+
 export const defaultFigTextBodyParser = bodyparser.text({
   type: ["text/jwt", "application/jwt", "application/fig", "text/plain"],
 });
 export const defaultFigJsonBodyParser = bodyparser.json();
 
-export default function createHandler(
+export default function createDefaultHandler(
   source: Fig,
-  warn: (message: string) => void = (message) => console.warn(message)
+  warn: (message: Error) => void = (message) => console.warn(message)
 ) {
   const chain = connect();
 
