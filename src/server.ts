241da816976d48f9e585cf586ceb6a48b982499d
Joshua Ohlman <codewriterohs@gmail.com>
2020-05-09 11:14:22 -0400

    Server should provide the query middleware by default

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import { Fig, Query, Record } from "./core";
 import connect, { IncomingMessage } from "connect";
 import bodyparser from "body-parser";
 import { ServerResponse } from "http";
+import { Request } from "express";
 
 // TODO: should verify keys
 
-export type FigRequest = IncomingMessage & {
+export type FigRequest = (Request | IncomingMessage) & {
   body?: string | string[];
   figMessages?: string[];
   figQuery?: Query;
@@ -47,15 +48,48 @@ export function formatMessages(messages: Record[]): string {
   return messages.map((m) => m.token).join("\n");
 }
 
-export function messageMiddleware(req: FigRequest, res: ServerResponse) {
+export function messageMiddleware(
+  req: FigRequest,
+  res: ServerResponse,
+  next: () => void
+) {
   if (typeof req.body === "string") {
     req.figMessages = req.body.split("\n").filter((message) => message);
   } else if (req.body instanceof Array) {
     req.figMessages = req.body.filter((message) => message);
   }
+
+  next();
 }
 
-export function queryMiddleware(req: FigRequest, res: ServerResponse) {}
+export function queryMiddleware(
+  req: FigRequest,
+  res: ServerResponse,
+  next: () => void
+) {
+  if ("query" in req) {
+    req.figQuery = {
+      limit: parseNumberFromQueryParam(req.query.limit),
+      after: parseIdFromQueryParam(req.query.after),
+    };
+  }
+  next();
+}
+
+export function parseNumberFromQueryParam(param: unknown): number | undefined {
+  if (typeof param === "string" && param.match(/^\d+$/)) {
+    const parsedLimit = parseInt(param);
+    if (!isNaN(parsedLimit)) {
+      return parsedLimit;
+    }
+  }
+}
+
+export function parseIdFromQueryParam(param: unknown): string | undefined {
+  if (typeof param === "string" && param.match(/^[a-zA-Z\-_]+$/)) {
+    return param;
+  }
+}
 
 export const defaultFigTextBodyParser = bodyparser.text({
   type: ["text/jwt", "application/jwt", "application/fig", "text/plain"],
@@ -71,6 +105,7 @@ export default function createDefaultHandler(
   chain.use(defaultFigTextBodyParser);
   chain.use(defaultFigJsonBodyParser);
   chain.use(messageMiddleware);
+  chain.use(queryMiddleware);
   chain.use(createFigHandler(source, warn));
 
   return chain;
